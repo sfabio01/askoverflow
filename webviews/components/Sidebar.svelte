05ad99faa9de6795f8c75220fb4749ed96ea50a2
Fabio Sabbion <fabiosabbion.apps@gmail.com>
2021-10-11 23:00:49 +0200

    added home page and search feature

--- webviews/components/Sidebar.svelte
+++ webviews/components/Sidebar.svelte
@@ -1,14 +1,58 @@
 <script lang="ts">
-    import { createHash } from "crypto";
-
     import { onMount } from "svelte";
+
+    const Page = {
+        home: "home",
+        ask: "ask",
+    };
 
     const key = "BS00p5ATAfS7dMN3HrcL9A((";
     let accessToken = "";
 
+    let query = "";
+    let results: Array<any> = [];
+    let askedOnce = false;
+
     let title = "";
     let body = "";
     let tags = "";
+
+    let screen = Page.home;
+
+    function navigateToAskPage() {
+        screen = Page.ask;
+    }
+
+    function navigateToHomePage() {
+        screen = Page.home;
+    }
+
+    async function onQueryChanged() {
+        if (query.length < 3) return;
+        let url = new URL("https://api.stackexchange.com/2.3/search/advanced");
+        url.searchParams.set("q", query);
+        url.searchParams.set("sort", "relevance");
+        url.searchParams.set("site", "stackoverflow");
+        console.log(url.toString());
+        const xhr = new XMLHttpRequest();
+        xhr.addEventListener("load", function (event) {
+            let data = JSON.parse(xhr.responseText);
+            console.log(data);
+            if (xhr.status == 400) {
+                tsvscode.postMessage({
+                    type: "onError",
+                    value: data.error_message,
+                });
+            }
+            if (xhr.status == 200) {
+                results = data.items;
+                askedOnce = true;
+            }
+        });
+
+        xhr.open("GET", url.toString());
+        xhr.send();
+    }
 
     async function postQuestion() {
         if (accessToken == "") {
@@ -89,35 +133,65 @@
     });
 </script>
 
-<h1>Ask a question</h1>
-<label for="title"><b>Title</b></label>
-<input
-    id="title"
-    placeholder="e.g. Is there an R function for finding the index of an element in a vector?"
-    minlength="15"
-    bind:value={title}
-/>
-
-<label for="body"><b>Body</b></label>
-<textarea
-    rows="15"
-    id="body"
-    style="resize: vertical;"
-    minlength="30"
-    bind:value={body}
-/>
-<p style="font-weight: 300;">```code``` **bold** *italic* >quote</p>
-
-<label for="tags"><b>Tags</b></label>
-<input id="tags" placeholder="e.g. (spring django laravel)" bind:value={tags} />
-
-<button on:click={postQuestion}>post question</button>
-
-<p style="font-weight: 300;">
-    <img
-        alt="svgImg"
-        src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHg9IjBweCIgeT0iMHB4Igp3aWR0aD0iMTYiIGhlaWdodD0iMTYiCnZpZXdCb3g9IjAgMCAyNCAyNCIKc3R5bGU9IiBmaWxsOiNmZmZmZmY7Ij4gICAgPHBhdGggZD0iTTEyLDJDNi40NzcsMiwyLDYuNDc3LDIsMTJzNC40NzcsMTAsMTAsMTBzMTAtNC40NzcsMTAtMTBTMTcuNTIzLDIsMTIsMnogTTEyLDE3TDEyLDE3Yy0wLjU1MiwwLTEtMC40NDgtMS0xdi00IGMwLTAuNTUyLDAuNDQ4LTEsMS0xaDBjMC41NTIsMCwxLDAuNDQ4LDEsMXY0QzEzLDE2LjU1MiwxMi41NTIsMTcsMTIsMTd6IE0xMi41LDloLTFDMTEuMjI0LDksMTEsOC43NzYsMTEsOC41di0xIEMxMSw3LjIyNCwxMS4yMjQsNywxMS41LDdoMUMxMi43NzYsNywxMyw3LjIyNCwxMyw3LjV2MUMxMyw4Ljc3NiwxMi43NzYsOSwxMi41LDl6Ij48L3BhdGg+PC9zdmc+"
+{#if screen == Page.home}
+    <button on:click={navigateToAskPage}>Ask Question</button>
+    <h1>Search on StackOverflow</h1>
+    <input
+        id="query"
+        placeholder="Search..."
+        bind:value={query}
+        on:change={onQueryChanged}
     />
-    You can use the <b>Insert code from current selection</b> command to append your
-    code to the body of the question
-</p>
+    <p style="font-weight: 300;">Press 'Enter' to search</p>
+    {#if askedOnce}
+        <h2>{results.length} results found</h2>
+    {/if}
+    {#each results as result}
+        <h3>
+            {result.title}
+            {#if result.is_answered}<span style="color: teal;">&#10004;</span
+                >{/if}
+        </h3>
+        <a href={result.link}>{result.link}</a>
+        <br /> <br />
+    {/each}
+{:else if screen == Page.ask}
+    <button on:click={navigateToHomePage}>back</button>
+    <h1>Ask a question</h1>
+    <label for="title"><b>Title</b></label>
+    <input
+        id="title"
+        placeholder="e.g. Is there an R function for finding the index of an element in a vector?"
+        minlength="15"
+        bind:value={title}
+    />
+
+    <label for="body"><b>Body</b></label>
+    <textarea
+        rows="15"
+        id="body"
+        style="resize: vertical;"
+        minlength="30"
+        bind:value={body}
+    />
+
+    <p style="font-weight: 300;">```code``` **bold** *italic* >quote</p>
+
+    <label for="tags"><b>Tags</b></label>
+    <input
+        id="tags"
+        placeholder="e.g. (spring django laravel)"
+        bind:value={tags}
+    />
+
+    <button on:click={postQuestion}>post question</button>
+
+    <p style="font-weight: 300;">
+        <img
+            alt="svgImg"
+            src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHg9IjBweCIgeT0iMHB4Igp3aWR0aD0iMTYiIGhlaWdodD0iMTYiCnZpZXdCb3g9IjAgMCAyNCAyNCIKc3R5bGU9IiBmaWxsOiNmZmZmZmY7Ij4gICAgPHBhdGggZD0iTTEyLDJDNi40NzcsMiwyLDYuNDc3LDIsMTJzNC40NzcsMTAsMTAsMTBzMTAtNC40NzcsMTAtMTBTMTcuNTIzLDIsMTIsMnogTTEyLDE3TDEyLDE3Yy0wLjU1MiwwLTEtMC40NDgtMS0xdi00IGMwLTAuNTUyLDAuNDQ4LTEsMS0xaDBjMC41NTIsMCwxLDAuNDQ4LDEsMXY0QzEzLDE2LjU1MiwxMi41NTIsMTcsMTIsMTd6IE0xMi41LDloLTFDMTEuMjI0LDksMTEsOC43NzYsMTEsOC41di0xIEMxMSw3LjIyNCwxMS4yMjQsNywxMS41LDdoMUMxMi43NzYsNywxMyw3LjIyNCwxMyw3LjV2MUMxMyw4Ljc3NiwxMi43NzYsOSwxMi41LDl6Ij48L3BhdGg+PC9zdmc+"
+        />
+        You can use the <b>Insert code from current selection</b> command to append
+        your code to the body of the question
+    </p>
+{/if}
